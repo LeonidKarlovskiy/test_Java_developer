d3213b45190d325cb0a69ea75bbc06241a61a984
Leonid Karlovskii <karlovskiyleo@gmail.com>
2023-07-10 11:16:42 +0300

    Start commit

---
+++ src/main/java/com/example/demo/model/Users.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+
+
+public class Users {
+
+    private Long id;
+    private String name;
+    private String surName;
+    private String email;
+    private String imageUrl;
+    private String status;
+
+}
