b758dd26abb9f244446c47dc76fc052f7c296d1a
Leonid Karlovskii <karlovskiyleo@gmail.com>
2023-07-10 11:24:03 +0300

    added mapping

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Users;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    @GetMapping
     public Users getUser(){
         return new Users(1l, "name", "surName", "email", "url", "false");
     }
