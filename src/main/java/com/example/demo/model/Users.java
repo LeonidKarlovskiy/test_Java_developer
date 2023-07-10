30d70dcab65856114fbc99e9f2d0d37331762af4
Leonid Karlovskii <karlovskiyleo@gmail.com>
2023-07-10 11:18:53 +0300

    annotations  for entity

--- src/main/java/com/example/demo/model/Users.java
+++ src/main/java/com/example/demo/model/Users.java
@@ -1,14 +1,32 @@
 package com.example.demo.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class Users {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false,unique = true)
     private Long id;
+    @Column(name = "name", nullable = false)
     private String name;
+    @Column(name = "surName", nullable = false)
     private String surName;
+    @Column(name = "email",unique = true, nullable = false)
     private String email;
+    @Column(name = "imageUrl",unique = false, nullable = false)
     private String imageUrl;
+    @Column(name = "status",unique = false, nullable = false)
     private String status;
 
 }
